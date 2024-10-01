3b374d59fff1bd9e2ef9bd136c4e43a571db2809
Jesse C. Mwesigwa <111117636+JesseCalebMwesigwa@users.noreply.github.com>
2024-10-01 17:43:17 +0300

    frames

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 function App(){
-  return <h1>Hello Jesse!</h1>
+  return (
+    // fragments
+    <>
+      <h1>Hello World!</h1>
+      <h2>Learning to code with React</h2>
+    </>
+  )
 }
 
 export default App
