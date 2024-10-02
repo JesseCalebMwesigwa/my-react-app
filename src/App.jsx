f13d9cb70be5bd277f8a002a0f991b86efda2e5b
Jesse C. Mwesigwa <111117636+JesseCalebMwesigwa@users.noreply.github.com>
2024-10-02 11:12:08 +0300

    multiple

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-function App(){
+// function App(){
+//   return (
+//     // fragments
+//     <>
+//       <h1>Hello World!</h1>
+//       <h2>Learning to code with React</h2>
+//     </>
+//   )
+// }
+
+// export default App
+
+// composing multiple components
+export default function ParentComponent() {
   return (
-    // fragments
     <>
-      <h1>Hello World!</h1>
-      <h2>Learning to code with React</h2>
+      <UserComponent />
+      <ProfileComponent />
+      <FeedComponent />
     </>
-  )
+  );
 }
 
-export default App
+function UserComponent() {
+  return <h1> User Component </h1>;
+}
+
+function ProfileComponent() {
+  return <h1> Profile Component </h1>;
+}
+
+function FeedComponent() {
+  return <h1> Feed Component</h1>;
+}
