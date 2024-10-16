d7277e56caf0857992b049d567d76eec883e4733
Jesse C. Mwesigwa <111117636+JesseCalebMwesigwa@users.noreply.github.com>
2024-10-16 16:58:02 +0300

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,6 @@
-// function App(){
-//   return (
-//     // fragments
-//     <>
-//       <h1>Hello World!</h1>
-//       <h2>Learning to code with React</h2>
-//     </>
-//   )
-// }
-
-// export default App
-
-// composing multiple components
-export default function ParentComponent() {
-  return (
-    <>
-      <UserComponent />
-      <ProfileComponent />
-      <FeedComponent />
-    </>
-  );
+function App() {
+  const myElement = <h1>Hello World</h1>
+  return myElement
 }
 
-function UserComponent() {
-  return <h1> User Component </h1>;
-}
-
-function ProfileComponent() {
-  return <h1> Profile Component </h1>;
-}
-
-function FeedComponent() {
-  return <h1> Feed Component</h1>;
-}
+// making sense of jsx
